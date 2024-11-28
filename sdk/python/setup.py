72db3852f32f8c69a66c5bac96c1ffb847659329
Sawyer <35255629+shaoyie@users.noreply.github.com>
2024-11-28 12:15:33 +0800

    fix: update version for python sdk upload

--- sdk/python/setup.py
+++ sdk/python/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ticos-client",
-    version="0.1.0",
+    version="0.1.1",
     author="Ticos Team",
     author_email="admin@tiwater.com",
     description="A client SDK for communicating with Ticos Server",
