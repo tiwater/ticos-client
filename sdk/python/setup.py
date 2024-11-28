386c87cece32bfcc2205a48810ad9f7c6c9eed54
Sawyer <35255629+shaoyie@users.noreply.github.com>
2024-11-28 12:29:36 +0800

    refactor: change to python style

--- sdk/python/setup.py
+++ sdk/python/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ticos-client",
-    version="0.1.1",
+    version="0.1.2",
     author="Ticos Team",
     author_email="admin@tiwater.com",
     description="A client SDK for communicating with Ticos Server",
---
+++ sdk/python/ticos_client/__init__.py
@@ -0,0 +1,1 @@
+from .ticos_client import TicosClient
